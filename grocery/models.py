ae0c59067fe0ed1b8475155f087107329ee169ca
Asim Poudel <asimdkt63@gmail.com>
2026-03-05 21:16:32 +0545

    update model and serializers

--- grocery/models.py
+++ grocery/models.py
@@ -1,3 +1,14 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-id']  # Latest first
